d14559b53934e380817626aefd25f9d2fe32e07b
whitelion <whitelion.b@outlook.com>
2023-05-10 16:44:39 +0800

    restore cursor position

--- nvim/lua/core/init.lua
+++ nvim/lua/core/init.lua
@@ -1,17 +1,18 @@
 local M = {}
 
 function M:init()
-  vim.g.mapleader = " "
-  vim.g.maplocalleader = " "
+	vim.g.mapleader = " "
+	vim.g.maplocalleader = " "
 
-  _G.icons = require("util.icons")
-  require("core.settings"):load_options()
+	_G.icons = require("util.icons")
+	require("core.settings"):load_options()
 
-  local bind = require("keymap.bind")
-  local keymap = require("core.mapping")
-  bind:load_keys(keymap)
+	local bind = require("keymap.bind")
+	local keymap = require("core.mapping")
+	bind:load_keys(keymap)
 
-  require("core.lazy")
+	require("core.lazy")
+	require("core.autocmds")
 end
 
 return M
